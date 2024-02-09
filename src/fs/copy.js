c4f19666a855a7226719e11800ecee1d6aea86f3
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-09 14:46:40 +0300

    feat: Add function in file copy.js, correct the error later

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { promises as fs } from 'fs';
+// Закончить асинхронную функцию, ошибка не известная, узнать почему не копируются файлы с одного католога в другой
 const copy = async () => {
-    // Write your code here 
+    const sourceDirectory = './files';
+    const destinationDirectory = './files_copy';
+    
+    try {
+        await fs.access(sourceDirectory);
+        await fs.access(destinationDirectory);
+        throw new Error('Каталоги уже существуют');
+    } catch (error) {
+        if (error.code !== 'ENOENT') throw new Error ('FS operation failed')
+    }
+
+    await fs.mkdir(destinationDirectory);
+    const files = await fs.readdir(sourceDirectory);
+
+    await Promise.all(files.map( async (file) => {
+        const sourceFile = '${sourceDirectory}/${file}';
+        const destinationFile = '${destinationDirectory}/${file}';
+        await fs.copyFile(sourceFile, destinationFile);
+    })); 
+    console.log('Все файлы скопированы успешно');
+
+    try {
+        await copy();
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
-await copy();
