c28bb0515f10e2b1ad221ec55056900df199f6b7
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-09 13:59:23 +0300

    feat: Completed file create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,16 @@
-
+import { promises as fs } from 'fs'; 
 const create = async () => {
-    // Write your code here 
+    const filePath = './files/fresh.txt';
+    const content = 'I am fresh and young\n';
+
+    try {
+        await fs.access(filePath);
+        throw new Error('Файла не сущществует');
+    } catch (error) {
+        if (error.code !== 'ENOENT') throw error;
+        await fs.writeFile(filePath, content);
+        console.log('Файл создан')
+    }
 };
 
 await create();
