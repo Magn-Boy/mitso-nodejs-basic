03fabfa35482fa2ec7bd297fe39a4128160f0bb9
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-09 19:35:11 +0300

    feat: Completed file rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const sourceFile = './files/wrongFilename.txt';
+    const destinationFile = './files/properFilename.md';
+
+    try {
+        await fs.access(sourceFile);
+        await fs.access(destinationFile);
+        throw new Error ('Файлы уже существуют');
+    } catch (error) {
+        if (error.code !== 'ENOENT') throw new Error ('FS operation failed');
+    }
+
+    try {
+        await fs.rename(sourceFile, destinationFile);
+        console.log('Файл успешно переименован');
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 };
 
-await rename();
+rename().catch(error => console.error(error.message));
