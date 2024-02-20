e20787cd6c696f7813b15aab98d3625e1c56cf23
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-20 20:18:24 +0300

    Changed file cjsToEsm

---
+++ src/modules/esm.mjs
@@ -0,0 +1,41 @@
+import path from 'path';
+import { release, version } from 'os' ;
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = import('./files/a.json').then(module => module.default);
+} else {
+    unknownObject = import('./files/b.json').then(module => module.default);
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${import.meta.sep}"`);
+
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${path.dirname (import.meta.url)}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+unknownObject.then(obj => {
+    console.log(obj);
+});
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {
+    unknownObject, myServer,
+};
+
