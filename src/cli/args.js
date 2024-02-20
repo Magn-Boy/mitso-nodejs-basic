6e50cab270cc5721efa0bf9c88a03306de59fc82
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-20 12:08:13 +0300

    feat: Finish file args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    process.argv.slice(2).forEach((arg, index, arr) => {
+        if (index % 2 === 0) {
+            console.log(`${arg} is ${arr[index + 1]}`);
+        }
+    }); 
 };
 
 parseArgs();
