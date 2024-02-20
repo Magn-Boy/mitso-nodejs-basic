513d50cf39cccdba49bce2404343c078084007fa
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-20 09:07:10 +0300

    feat: Finished file env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const mitsoVariables = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('MITSO_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+    console.log(mitsoVariables); 
 };
 
 parseEnv();
