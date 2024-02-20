2b240f056d674ae02daa3da4bc465c989b7b715e
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-02-20 08:48:02 +0300

    feat: Finished file read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { error } from 'console';
+import { promises as fs } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = './files/fileToRead.txt';
+    try {
+        await fs.access(fileToRead);
+    } catch (error){
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const content = await fs.readFile(fileToRead, 'utf-8');
+        console.log('Содержимое файла:');
+        console.log(content);
+    }catch (error) {
+        console.log('Ошибка при чтении файла: ${error.message}')
+    }
 };
 
-await read();
+read().catch(error => console.error(error.message));
