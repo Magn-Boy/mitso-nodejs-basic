924383338c7e9cf36ce6bf5bee6995dab5f9dad0
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-04-08 21:35:57 +0300

    feat: File write.js complete

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream('./files/fileToWrite.txt', { flags: 'a' });
+    process.stdin.pipe(writeStream);
+    console.log('Enter data to write to file (press Ctrl + D to finish):');
 };
 
 await write();
