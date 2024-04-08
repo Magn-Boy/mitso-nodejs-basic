bcdb478cdc6aee5e499a4d72e8fd52b222a44c5f
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-04-08 22:45:24 +0300

    feat: File cp.js and script.js complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'child_process';
 
+const spawnChildProcess = async (args) => {
+    const childProcess = spawn('node', ['files/script.js', ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'] 
+    });
+
+    
+    childProcess.stdout.on('data', (data) => {
+
+        process.stdout.write(data);
+    });
+
+    
+    process.stdin.pipe(childProcess.stdin);
+};
+const args = ['argument1', 'argument2', 'argument3'];
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(args);
