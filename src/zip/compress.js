37d1a9d915299445b9a4de96c69a31c0834700cb
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-04-08 21:58:54 +0300

    feat: File compress.js comlpete

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream('./files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('archive.gz');
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File compressed successfully');
+    });
+
+    writeStream.on('error', (error) => {
+        console.error(`Error compressing file: ${error.message}`);
+    });
 };
 
 await compress();
