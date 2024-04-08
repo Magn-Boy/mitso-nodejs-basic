c7931b30145a4437fc1f0a8d66cd829521fdcb6f
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-04-08 22:07:09 +0300

    feat: File decompress.js complete

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const gzipStream = fs.createReadStream('archive.gz');
+    const decompressStream = zlib.createGunzip();
+    const writeStream = fs.createWriteStream('./files/fileToCompress.txt', { flags: 'a' });
+
+    gzipStream.pipe(decompressStream).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Decompression completed');
+    });
+
+    writeStream.on('error', (error) => {
+        console.error(`Error writing decompressed file: ${error.message}`);
+    });
 };
 
 await decompress();
