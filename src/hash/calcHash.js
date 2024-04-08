72de4ffbc5a0c69c424e12daf37e059988c0f49c
Magn-Boy <andruha.vasilev.2004@mail.ru>
2024-04-08 21:12:17 +0300

    feat: File calcHash.js complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = './files/fileToCalculateHashFor.txt';
+    const hash = createHash('sha256');
+    const stream = createReadStream(fileName);
+
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        stream.on('end', () => {
+            const hashResult = hash.digest('hex');
+            console.log(`SHA256 hash of ${fileName}: ${hashResult}`);
+            resolve();
+        });
+
+        stream.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
 
 await calculateHash();
